36033bfa7bc1e19489a9829ad9a38576fe9032e3
emmanuel <oemmanuel.uwadiae@gmail.com>
2024-09-06 14:47:50 +0100

    adding ability to publish drafts and delete posts

--- blog_app/views.py
+++ blog_app/views.py
@@ -22,7 +22,6 @@ def post_new(request):
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
-            post.published_date = timezone.now()
             post.save()
             return redirect("post_detail", pk=post.pk)
     else:
@@ -38,10 +37,28 @@ def post_edit(request, pk):
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
-            post.published_date = timezone.now()
             post.save()
             return redirect("post_detail", pk=post.pk)
     else:
         form = PostForm(instance=post)
 
     return render(request, "blog_app/post_edit.html", {"form": form})
+
+
+def post_draft_list(request):
+    posts = Post.objects.filter(published_date__isnull=True).order_by("created_date")
+    return render(request, "blog_app/post_draft_list.html", {"posts": posts})
+
+
+def post_publish(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        post.publish()
+    return redirect("post_detail", pk=pk)
+
+
+def post_remove(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        post.delete()
+    return redirect("post_list")
